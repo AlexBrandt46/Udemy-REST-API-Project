0a91819d457830f34d540a613869862a2d06f8ef
AJ Brandt <ajb3155@gmail.com>
2023-02-22 00:45:43 -0500

    Finished creating model objects

--- models/item.py
+++ models/item.py
@@ -4,5 +4,7 @@ class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
-
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, unique=False, nullable=False)
     
