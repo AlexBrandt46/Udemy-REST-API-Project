5acc80a8087fe0d6dad6ea281173cd992e979a67
AJ Brandt <ajb3155@gmail.com>
2023-04-16 22:33:23 -0400

    Added login endpoint

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
 
 from db import db
 from models import UserModel
@@ -41,3 +42,18 @@ class User(MethodView):
         
         return { "message": "User deleted." }, 200
         
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+        
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id)
+            return { "access_token": access_token }
+        else:
+            abort(401, message="Invalid credentials.")
